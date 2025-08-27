c7ad4d4836f68d6ecdd02f5cefa8a9c3b027bf82
jonabergamo <jonathanbergamo16@gmail.com>
2025-08-27 08:28:08 -0300

    Update .gitignore and enhance invoice generation script to create output directory and improve PDF handling

--- generate_invoice.sh
+++ generate_invoice.sh
@@ -25,6 +25,10 @@ CURRENT_YEAR=$(date +'%Y')
 # Nome do arquivo PDF gerado
 PDFNAME="Invoice_${CURRENT_MONTH}_${CURRENT_YEAR}_#${NEXT_INVOICE_NUMBER}.pdf"
 
+# Pasta de saída
+INVOICE_DIR="${TEMPLATE_DIR}/invoices"
+mkdir -p "$INVOICE_DIR"
+
 # Caminho do template LaTeX
 TEX_TEMPLATE="${TEMPLATE_DIR}/invoice_template.tex"
 
@@ -50,8 +54,7 @@ escape_sed() {
 # Criando um arquivo temporário .tex com as variáveis substituídas
 TEMP_TEX="${TEMPLATE_DIR}/invoice_temp.tex"
 
-# Substituindo variáveis do template com as variáveis escapadas
-echo "Substituindo variáveis no template..."
+# Substituindo variáveis do template
 sed "s/{{INVOICE_NUMBER}}/${NEXT_INVOICE_NUMBER}/g" "$TEX_TEMPLATE" | \
 sed "s/{{CURRENT_YEAR}}/${CURRENT_YEAR}/g" | \
 sed "s/{{CURRENT_MONTH}}/${CURRENT_MONTH}/g" | \
@@ -66,7 +69,6 @@ sed "s/{{CURRENCY}}/$(escape_sed "$CURRENCY")/g" | \
 sed "s/{{AMOUNT}}/$(escape_sed "$AMOUNT")/g" | \
 sed "s/{{BILL_FROM}}/$(escape_sed "$BILL_FROM")/g" | \
 sed "s/{{BILL_TO}}/$(escape_sed "$BILL_TO")/g" | \
-
 sed "s/{{BANK_ADDRESS}}/$(escape_sed "$BANK_ADDRESS")/g" | \
 sed "s/{{IBAN}}/$(escape_sed "$IBAN")/g" | \
 sed "s/{{SWIFT}}/$(escape_sed "$SWIFT")/g" | \
@@ -74,18 +76,16 @@ sed "s/{{INTERMEDIARY_BANK_NAME}}/$(escape_sed "$INTERMEDIARY_BANK_NAME")/g" | \
 sed "s/{{INTERMEDIARY_BANK_SWIFT}}/$(escape_sed "$INTERMEDIARY_BANK_SWIFT")/g" | \
 sed "s/{{SUPPORT_EMAIL}}/$(escape_sed "$SUPPORT_EMAIL")/g" > "$TEMP_TEX"
 
-# Verifique o conteúdo gerado do arquivo .tex
-echo "Arquivo .tex gerado:"
-cat "$TEMP_TEX"  # Isso vai ajudar a verificar se a substituição ocorreu corretamente
-
 # Compilando o arquivo .tex para gerar o PDF
 pdflatex -output-directory="$TEMPLATE_DIR" "$TEMP_TEX" && \
-mv "${TEMPLATE_DIR}/invoice_temp.pdf" "$PDFNAME"
+mv "${TEMPLATE_DIR}/invoice_temp.pdf" "${INVOICE_DIR}/${PDFNAME}"
 
 # Limpando os arquivos auxiliares
 rm -f "$TEMP_TEX" "${TEMPLATE_DIR}/invoice_temp.aux" "${TEMPLATE_DIR}/invoice_temp.log" "${TEMPLATE_DIR}/invoice_temp.out"
 
-
 # Verificando se o PDF foi gerado com sucesso
-if [ ! -f "$PDFNAME" ]; then
-    echo "Error: PDF generation failed. Check the .log fi
+if [ -f "${INVOICE_DIR}/${PDFNAME}" ]; then
+    echo "PDF gerado com sucesso em: ${INVOICE_DIR}/${PDFNAME}"
+else
+    echo "Error: PDF generation failed."
+fi
